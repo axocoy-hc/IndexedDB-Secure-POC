974a0f5683818178ffac2833e1c38f5b28342344
Antonio Xocoy <axocoy@harriscomputer.com>
2025-03-19 07:14:11 -0600

    Transform CryptoKey to JSON to reuse

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {EncryptService} from '../../services/encryption.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -6,40 +6,41 @@ import {switchMap} from 'rxjs/operators';
 import {IndexedDbService} from '../../services/indexed-db.service';
 
 @Component({
-  selector: 'app-form',
-  standalone: false,
-  templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  selector: 'app-form', standalone: false, templateUrl: './form.component.html', styleUrl: './form.component.css'
 })
 export class FormComponent {
 
   public formGroup: FormGroup;
   public idGenerated?: number;
+  public generalKeyJSON?: string;
   private generalKey?: { derivedKey: CryptoKey; iv: Uint8Array };
+  public itemsList?: any[];
 
-  constructor(private authService: AuthService,
-              private encryptService: EncryptService,
-              private indexedDbService: IndexedDbService,
-              private formBuilder: FormBuilder) {
+  constructor(private authService: AuthService, private encryptService: EncryptService, private indexedDbService: IndexedDbService, private formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
       content: ['', Validators.required]
     })
   }
 
   onSubmit() {
-    const { content } = this.formGroup.value;
+    const {content} = this.formGroup.value;
     const email = this.authService.userAuth()?.email
     const userId = this.authService.userAuth()?.userId
-    console.log(content, userId, email);
+    console.log('passphrase used', userId, email);
     this.encryptService.generateAndStoreKey(`${email}${userId}`)
-      .pipe(
-        switchMap(key => {
+      .pipe(switchMap(key => {
+        if (!this.generalKey) {
           this.generalKey = key;
-          console.log(JSON.stringify(key));
-          return this.encryptService.encryptData(content, key.derivedKey, key.iv)
-        }),
-        switchMap(value => this.indexedDbService.addItem(value))
-      )
+        }
+        console.log('this.generalKey', this.generalKey);
+        this.encryptService.transformToJsonWebKey(this.generalKey.derivedKey)
+          .subscribe(jsonDerivatedKey => {
+            this.generalKeyJSON = (JSON.stringify({
+              derivedKey: jsonDerivatedKey, iv: this.encryptService.arrayBufferToBase64(this.generalKey!.iv),
+            }));
+          })
+        return this.encryptService.encryptData(content, this.generalKey.derivedKey, this.generalKey.iv)
+      }), switchMap(value => this.indexedDbService.addItem(value)))
       .subscribe(id => {
         this.idGenerated = id;
         setTimeout(() => {
@@ -50,21 +51,30 @@ export class FormComponent {
   }
 
   refreshList(): void {
-    const email = this.authService.userAuth()?.email
-    const userId = this.authService.userAuth()?.userId
-    console.log(userId, email);
-    // this.encryptService.generateAndStoreKey(`${email}${userId}`)
-    //   .subscribe(key => {
-        this.indexedDbService.getAll()
-          .subscribe(valueDe => {
-            valueDe.forEach(v => {
-              console.log('VALUE', v)
-              this.encryptService.decryptData(v, this.generalKey!.derivedKey, this.generalKey!.iv)
-                .subscribe(result => {
-                console.log('VALUE DECRYPTED', result)
-              });
+    this.itemsList = [];
+    this.indexedDbService.getAll()
+      .subscribe(valueDe => {
+        valueDe.forEach(v => {
+          this.encryptService.decryptData(v, this.generalKey!.derivedKey, this.generalKey!.iv)
+            .subscribe(result => {
+              this.itemsList?.push(result);
+              console.log('VALUE DECRYPTED', result)
             });
         });
-    //})
+      });
+  }
+
+  public loadKeyFromJSON(): void {
+    const jwk = JSON.parse(this.generalKeyJSON as string);
+    console.log('jwk', jwk)
+    this.encryptService.transformToCryptoKey(jwk.derivedKey)
+      .subscribe(cryptoKey => {
+      this.generalKey = {
+        iv: this.encryptService.base64ToUInt8Array(jwk.iv),
+        derivedKey: cryptoKey
+      }
+      console.log('loadKeyFromJSON', this.generalKey);
+      this.refreshList();
+    });
   }
 }
--- src/services/encryption.service.ts
+++ src/services/encryption.service.ts
@@ -1,10 +1,12 @@
-import {from, map, Observable} from 'rxjs';
+import {catchError, from, map, Observable} from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 export class EncryptService {
+  private readonly alg = 'AES-GCM'
+
   generateAndStoreKey(passphrase: string): Observable<{ derivedKey: CryptoKey, iv: Uint8Array }> {
     return from(crypto.subtle.generateKey(
-      { name: 'AES-GCM', length: 256 },
+      { name: this.alg, length: 256 },
       true,
       ['encrypt', 'decrypt']
     )).pipe(
@@ -20,7 +22,7 @@ export class EncryptService {
           switchMap(importedKey => from(crypto.subtle.deriveKey(
             { name: 'PBKDF2', salt, iterations: 100000, hash: 'SHA-256' },
             importedKey,
-            { name: 'AES-GCM', length: 256 },
+            { name: this.alg, length: 256 },
             true,
             ['encrypt', 'decrypt']
           ))),
@@ -28,7 +30,7 @@ export class EncryptService {
             switchMap(exportedMainKey => {
               const iv = crypto.getRandomValues(new Uint8Array(12));
               return from(crypto.subtle.encrypt(
-                { name: 'AES-GCM', iv },
+                { name: this.alg, iv },
                 derivedKey,
                 exportedMainKey
               )).pipe(
@@ -44,7 +46,7 @@ export class EncryptService {
   encryptData(data: string, key: CryptoKey, iv: Uint8Array): Observable<ArrayBuffer> {
     const encodedData = new TextEncoder().encode(data);
     return from(crypto.subtle.encrypt(
-      { name: 'AES-GCM', iv },
+      { name: this.alg, iv },
       key,
       encodedData
     ));
@@ -52,11 +54,53 @@ export class EncryptService {
 
   decryptData(encryptedData: ArrayBuffer, key: CryptoKey, iv: Uint8Array): Observable<string> {
     return from(crypto.subtle.decrypt(
-      { name: 'AES-GCM', iv },
+      { name: this.alg, iv },
       key,
       encryptedData
     )).pipe(
-      map(decryptedData => new TextDecoder().decode(decryptedData))
+      map(decryptedData => new TextDecoder().decode(decryptedData)),
+      catchError(err => {
+        console.error('Error decrypt:', err);
+        throw err;
+      })
+    );
+  }
+
+  public arrayBufferToBase64(uint8Array: Uint8Array): string {
+    const binaryString = Array.from(uint8Array).map(byte => String.fromCharCode(byte)).join('');
+    return btoa(binaryString);
+  }
+
+  public base64ToUInt8Array(base64: string): Uint8Array {
+    const binaryString = atob(base64);
+    const uint8Array = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+      uint8Array[i] = binaryString.charCodeAt(i);
+    }
+    return uint8Array;
+  }
+
+  public transformToJsonWebKey(key: CryptoKey) {
+    return from(crypto.subtle.exportKey('jwk', key)).pipe(
+      catchError(err => {
+        console.error('Error exporting key:', err);
+        throw err;
+      })
+    );
+  }
+
+  public transformToCryptoKey(jwkKey: JsonWebKey) {
+    return from(crypto.subtle.importKey(
+      'jwk',
+      jwkKey,
+      { name: this.alg },
+      true,
+      ['encrypt', 'decrypt']
+    )).pipe(
+      catchError(err => {
+        console.error('Error importing key:', err);
+        throw err;
+      })
     );
   }
 }
